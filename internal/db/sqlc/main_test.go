88a27a0d7f289ad0041f4b58cf21a3b9c2a19eb7
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-10-02 19:30:09 +0200

    Add viper to load config from a .env file

--- internal/db/sqlc/main_test.go
+++ internal/db/sqlc/main_test.go
@@ -7,16 +7,17 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v4"
+	"github.com/mateoradman/tempus/util"
 )
 
 var testQueries *Queries
 
-const (
-	dbSource = "postgresql://root:secret@localhost:5432/tempus?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	conn, err := pgx.Connect(context.Background(), dbSource)
+	config, err := util.LoadConfig("../../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := pgx.Connect(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
